9347f25a042f7c0f21361cdd36b0dddc0e7fe1a9
Riya Singh <rsingh2@affinionds.com>
2022-11-07 16:17:16 +0530

    changed to js

--- wdio.conf.js
+++ wdio.conf.js
@@ -18,13 +18,13 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        // './lib/specs/component.spec.js',
-        './tests/specs/component.spec.ts',
+         './lib/specs/component.spec.js',
+        //'./tests/specs/component.spec.ts',
     ],
     suites: {
         wdio1: [
-            //'./lib/specs/component.spec.js',
-            './tests/specs/component.spec.ts',
+            './lib/specs/component.spec.js',
+            //'./tests/specs/component.spec.ts',
         ]
     },
     // Patterns to exclude.
